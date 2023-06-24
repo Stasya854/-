57e322fb8e59c546f1c797e469bba76c805f8dc4
Stasya854 <135975850+Stasya854@users.noreply.github.com>
2023-06-24 10:55:29 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = "Abc123Def456";
+        string resultString = GetDigitsString(inputString);
+
+        Console.WriteLine($"Input string: {inputString}");
+        Console.WriteLine($"Result string: {resultString}");
+        Console.WriteLine($"Number of digits: {resultString.Length}");
+    }
+
+    static string GetDigitsString(string input)
+    {
+        string digitsString = "";
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                digitsString += c;
+            }
+        }
+
+        return digitsString;
+    }
+}
